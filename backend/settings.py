ab9f80d12c3d2b8300f773a26dd5a2e846ae8f9a
mauricio <mauricio_bo@hotmail.com>
2024-08-24 16:17:14 -0300

    chage DDBB

--- backend/settings.py
+++ backend/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djecommerce',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
